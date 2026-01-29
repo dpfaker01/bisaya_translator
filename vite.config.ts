04561a666495ab63a0fd929a88c895c23ce6549b
dpfaker01 <112249907+dpfaker01@users.noreply.github.com>
2026-01-30 02:11:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This tells GitHub Pages where to find your files
-  base: "/bisaya_translator/",
+  // IMPORTANT: Make sure this matches your repository name exactly
+  base: "/bisaya_translator/", 
   server: {
     port: 3000,
     host: '0.0.0.0',
